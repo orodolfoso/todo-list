d386414f8346aa1dc0f1a1ce83f9efdd90373bc7
orodolfoso <orodolfoso.dev@gmail.com>
2025-04-01 18:26:38 -0300

    Inicinado o JS

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,46 @@
-console.log('Olá!')
+// Seleção dos elementos
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('#todo-list')
+const editForm = document.querySelector('#edit-form')
+const editInput = document.querySelector('#edit-input')
+const cancelEditBtn = document.querySelector('#cancel-edit-btn')
+// Funções
+const saveTodo = (text) => {
+    const todo = document.createElement('div')
+    todo.classList.add('todo')
+
+    const todoTitle = document.createElement('h3')
+    todoTitle.innerText = text
+    todo.appendChild = (todoTitle)
+   
+
+    const doneBtn = document.createElement('button')
+    doneBtn.classList.add('finish-todo')
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit-todo')
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const delteBtn = document.createElement('button')
+    delteBtn.classList.add('remove-todo')
+    delteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(delteBtn)
+
+    todoList.appendChild(todo)
+}
+// Eventos
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const  inputValue = todoInput.value
+
+    if(inputValue) {
+        //save
+        saveTodo(inputValue)
+    }
+})
